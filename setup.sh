f9c2a75d594b3e17b580e1acc609160f6f05d519
Jang Yoon seo <123075825+jy00nse0@users.noreply.github.com>
2026-02-01 19:23:18 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,4 @@ cd /root
 git clone https://github.com/jy00nse0/Transformer_2.git
 conda create -n transformer_py python=3.10 -y
 conda activate transformer_py
+pip install torch datasets transformers
